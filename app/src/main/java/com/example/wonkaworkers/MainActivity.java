b34da47780b21ec2d96d180df74c0a5261281561
Daniel Rosquellas Montero <Rosky@iMac-de-Daniel.home>
2019-12-30 06:27:32 +0100

    Data extracted and correctly showed, spinner well-formed without redundancies

--- app/src/main/java/com/example/wonkaworkers/MainActivity.java
+++ app/src/main/java/com/example/wonkaworkers/MainActivity.java
@@ -2,13 +2,182 @@ package com.example.wonkaworkers;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.gson.Gson;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements RadioGroup.OnCheckedChangeListener {
+
+    ListAdapter stylazer;
+    RadioGroup genderFilter;
+    RadioButton all;
+    RadioButton male;
+    RadioButton female;
+    Spinner spnProfessions;
+    ListView listWorkers;
+
+    ArrayList crew;
+    List<String> professions = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listWorkers = findViewById(R.id.listWorkers);
+        genderFilter = findViewById(R.id.gender_filter);
+        all = findViewById(R.id.all);
+        male = findViewById(R.id.male);
+        female = findViewById(R.id.female);
+        spnProfessions = findViewById(R.id.spnr_professions);
+        genderFilter.setOnCheckedChangeListener(this);
+
+        new getJsonTask().execute("https://2q2woep105.execute-api.eu-west-1.amazonaws.com/napptilus/oompa-loompas?page=1");
+
     }
+
+    @Override
+    public void onCheckedChanged(RadioGroup radioGroup, int i) {
+
+    }
+
+    private class getJsonTask extends AsyncTask<String,String,String> {
+
+
+        @Override
+        protected String doInBackground(String... strings) {
+
+            HttpURLConnection connection = null;
+            BufferedReader reader = null;
+
+            try {
+                URL url = new URL(strings[0]);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+
+                InputStream stream = connection.getInputStream();
+
+                reader = new BufferedReader(new InputStreamReader(stream));
+
+                StringBuilder buffer = new StringBuilder();
+                String line;
+
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line).append("\n");
+                }
+
+                return buffer.toString();
+
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (connection != null) {
+                    connection.disconnect();
+                }
+                try {
+                    if (reader != null) {
+                        reader.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+
+            String jSonIntel = result;
+            OompaBasicInfo[] workersList;
+
+
+            if (!jSonIntel.startsWith("[") && (!jSonIntel.endsWith("]"))) {
+                jSonIntel = jSonIntel.substring(jSonIntel.indexOf('['), jSonIntel.length() - 2);
+            }
+
+            Gson gson = new Gson();
+
+            workersList = gson.fromJson(jSonIntel,OompaBasicInfo[].class);
+
+            crew = new ArrayList<>(Arrays.asList(workersList));
+
+            professions = getProfessions(crew);
+
+            createSpinner(professions);
+
+            makeList(crew);
+
+        }
+    }
+
+    private List getProfessions(ArrayList<OompaBasicInfo> workers){
+
+        List<String> ocupations = new ArrayList<>();
+
+        for (int i=0;i<workers.size();i++){
+
+            OompaBasicInfo worker = workers.get(i);
+
+            ocupations.add(worker.getProfession());
+
+        }
+
+        ocupations = depercateRedundancies(ocupations);
+
+        return ocupations;
+
+    }
+
+    private List depercateRedundancies(List redundantItems){
+
+        List<String> finalList = new ArrayList<>();
+
+        finalList.add("All");
+
+        HashSet<String> hashSet = new HashSet<>();
+        hashSet.addAll(redundantItems);
+        finalList.addAll(hashSet);
+
+        return finalList;
+
+    }
+
+    private void createSpinner (List elementsToShow){
+
+        final ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_spinner_item
+                ,elementsToShow);
+
+        spinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spnProfessions.setAdapter(spinnerArrayAdapter);
+
+    }
+
+    private void makeList (ArrayList<OompaBasicInfo> workersToShow){
+
+        stylazer = new ListAdapter(getApplicationContext(),R.layout.show_item,R.id.black,workersToShow);
+
+        listWorkers.setAdapter(stylazer);
+
+    }
+
 }
